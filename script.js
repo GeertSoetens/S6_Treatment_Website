05a23dc67769c0ebd43fed4410e34a35bd72840e
Soetens,Geert G. <g.soetens@student.fontys.nl>
2025-03-24 22:32:37 +0100

    update 4 lool let's go

---
+++ script.js
@@ -0,0 +1,35 @@
+const overlaybackground = document.getElementById("overlay");
+const videothumbnail = document.getElementById("videothumbnail");
+const video = document.getElementById("video");
+
+// Close overlay on load
+let overlayIsOpen = false;
+overlaybackground.style.display = "none";
+
+// When activated, toggles lightbox
+function toggleLightbox() {
+
+    if (overlayIsOpen) {
+        console.log("CLOSING OVERLAY BABY!!!");
+        // Close overlay
+        overlaybackground.style.display = "none";
+        overlayIsOpen = false;
+
+        // Pause video, if it is playing
+        video.pause();
+        
+        return;
+    }
+
+    // Open overlay
+    overlaybackground.style.display = "flex";
+    overlayIsOpen = true;
+}
+
+// All elements that toggle lightbox
+let toggleElements = [overlaybackground, videothumbnail];
+
+// Add event listeners
+toggleElements.forEach((element) => {
+    element.addEventListener("click", toggleLightbox);
+});
